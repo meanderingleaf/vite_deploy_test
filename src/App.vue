623ed7a36cabfedbe89395f995fba6d44790f854
Travis Faas <travis.faas@gmail.com>
2025-03-09 19:35:18 -0400

    Forst

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Button from "primevue/button"
+import Card from "primevue/card"
+
+const images = ["img/vue.svg", "img/absart.jpg"]
+
+</script>
+
+<template>
+
+  <Card v-for="image in images" :key="image">
+
+    <template #content>
+      <img :src="image" />
+    </template>
+  </Card>
+  
+</template>
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  base: "https://meanderingleaf.github.io/vite_deploy_test"
+})
